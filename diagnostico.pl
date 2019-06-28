3acec3422a0837810da9bad2a4e16abdd5a152b2
DanielQuintana <danielquintana90@gmail.com>
2019-06-27 21:56:20 -0500

    agregando relación sintoma-enfermedad

--- diagnostico.pl
+++ diagnostico.pl
@@ -1,3 +1,31 @@
-% Sistema experto para el diagnóstico de transtornos del neurodesarrollo
+% Sistema experto para el diagnostico de transtornos del neurodesarrollo
+
+es_sintoma_de(dificultad-prestar-atencion, trastorno-del-deficit-de-atencion-e-hiperactividad).
+es_sintoma_de(dejar-actividades-sin-terminar, trastorno-del-deficit-de-atencion-e-hiperactividad).
+es_sintoma_de(no-puede-permanecer-sentado, trastorno-del-deficit-de-atencion-e-hiperactividad).
+es_sintoma_de(inquieto-distintos-ambientes, trastorno-del-deficit-de-atencion-e-hiperactividad).
+es_sintoma_de(no-puede-organizarse, trastorno-del-deficit-de-atencion-e-hiperactividad).
+es_sintoma_de(no-piensa-en-consecuencias, trastorno-del-deficit-de-atencion-e-hiperactividad).
+es_sintoma_de(retraso-adquisicion-lenguaje, trastorno-de-espectro-autista).
+es_sintoma_de(solo-dice-algunas-palabras, trastorno-de-espectro-autista).
+es_sintoma_de(movimientos-repetitivos, trastorno-de-espectro-autista).
+es_sintoma_de(fijacion-temas-especificos, trastorno-de-espectro-autista).
+es_sintoma_de(rutinario-muy-ordenado, trastorno-de-espectro-autista).
+es_sintoma_de(sensibilidad-ruidos-fuertes, trastorno-de-espectro-autista).
+es_sintoma_de(dificultad-mantener-contacto-visual, trastorno-de-espectro-autista).
+es_sintoma_de(dificultad-relacionarse-personas, trastorno-de-espectro-autista).
+es_sintoma_de(dificultad-lectura-escritura-calculo, trastorno-de-aprendizaje).
+es_sintoma_de(pobre-rendimiento-escolar, trastorno-de-aprendizaje).
+es_sintoma_de(dificultad-comprension-lectora, trastorno-de-aprendizaje).
+es_sintoma_de(dificultad-aprendizaje, trastorno-de-aprendizaje).
+es_sintoma_de(dificultad-deletrear-palabras, trastorno-de-aprendizaje).
+es_sintoma_de(dificultad-calculos-aritmeticos, trastorno-de-aprendizaje).
+es_sintoma_de(dificultad-adquirir-habla, trastorno-del-desarrollo-del-habla-y-del-lenguaje).
+es_sintoma_de(omisiones-distorciones-fonemas-habla, trastorno-del-desarrollo-del-habla-y-del-lenguaje).
+es_sintoma_de(dificultad-elaborar-frases, trastorno-del-desarrollo-del-habla-y-del-lenguaje).
+es_sintoma_de(dificultad-asignar-nombres-objetos-familiares, trastorno-del-desarrollo-del-habla-y-del-lenguaje).
+
+
+
 
 
